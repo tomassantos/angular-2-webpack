407a9876bab7fbfde6d58cdf26eb56afe903b81c
tomas <tomas.santos@email.com>
2017-06-01 15:47:16 -0400

    added HtmlWebpackPlugin. It auto generates index.html split imports. cleaned webpack config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-  entry: './src/main.ts',
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    polyfills: './polyfills.ts',
+    vendor: './vendor.ts',
+    dashboard:'./index.ts'
+  },
   output: {
-    filename: './src/bundle.js'
- },
- resolve: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  resolve: {
     extensions: ['.ts', '.tsx', '.js'] 
- },
- module: {
+  },
+  module: {
     loaders: [
       { test: /\.tsx?$/, loader: 'ts-loader' }
     ]
- }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      hash: true
+    })
+  ]
 };
